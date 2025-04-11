8cbe089d200bc75ea263a2342bd228464dfb133a
Nate Getu <nathanielgetu@gmail.com>
2025-04-11 18:04:34 -0400

    Removed junk files

--- terraform/generated/aws/iam/iam_instance_profile.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_iam_instance_profile" "tfer--S3DynamoDBFullAccessRole" {
-  name = "S3DynamoDBFullAccessRole"
-  path = "/"
-  role = "S3DynamoDBFullAccessRole"
-}
--- terraform/generated/aws/iam/iam_user_policy_attachment.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_iam_user_policy_attachment" "tfer--Admin_AdministratorAccess" {
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-  user       = "Admin"
-}
