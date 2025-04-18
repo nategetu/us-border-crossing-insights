f09323190773900f3b13b3c5df80cd159c38adaa
Nate Getu <nathanielgetu@gmail.com>
2025-04-18 13:46:30 -0400

    added principal_ports lat and long

--- airflow/dags/sql/transform_tables.sql
+++ airflow/dags/sql/transform_tables.sql
@@ -6,47 +6,48 @@ create table bcbdb.prod.port_tonnage as (
         pp.port,
         pp.port_name,
         pp.total as total_tonnage,
-        bc.latitude,
-        bc.longitude,
-        cast(pp.foreign_ as FLOAT) / cast(pp.total as FLOAT) as foreign_pct,
+        pp.latitude1 as latitude,
+        pp.longitude1 as longitude,
+        cast(pp.foreign as FLOAT) / cast(pp.total as FLOAT) as foreign_pct,
         cast(pp.domestic as FLOAT) / cast(pp.total as FLOAT) as domestic_pct,
         cast(pp.imports as FLOAT) / cast(pp.total as FLOAT) as import_pct,
         cast(pp.exports as FLOAT) / cast(pp.total as FLOAT) as export_pct,
-        case foreign_pct
-            when between 0 and 0.2 then '0-20%'
-            when between 0.2 and 0.4 then '20-40%'
-            when between 0.4 and 0.6 then '40-60%'
-            when between 0.6 and 0.8 then '60-80%'
-            when between 0.8 and 1 then '80-100%'
+        case 
+            when foreign_pct between 0 and 0.2 then '0-20%'
+            when foreign_pct between 0.2 and 0.4 then '20-40%'
+            when foreign_pct between 0.4 and 0.6 then '40-60%'
+            when foreign_pct between 0.6 and 0.8 then '60-80%'
+            when foreign_pct between 0.8 and 1 then '80-100%'
             else 'N/A'
         end as binned_foreign_pct,
-        case domestic_pct
-            when between 0 and 0.2 then '0-20%'
-            when between 0.2 and 0.4 then '20-40%'
-            when between 0.4 and 0.6 then '40-60%'
-            when between 0.6 and 0.8 then '60-80%'
-            when between 0.8 and 1 then '80-100%'
+        case 
+            when domestic_pct between 0 and 0.2 then '0-20%'
+            when domestic_pct between 0.2 and 0.4 then '20-40%'
+            when domestic_pct between 0.4 and 0.6 then '40-60%'
+            when domestic_pct between 0.6 and 0.8 then '60-80%'
+            when domestic_pct between 0.8 and 1 then '80-100%'
             else 'N/A'
         end as binned_domestic_pct,
-        case import_pct
-            when between 0 and 0.2 then '0-20%'
-            when between 0.2 and 0.4 then '20-40%'
-            when between 0.4 and 0.6 then '40-60%'
-            when between 0.6 and 0.8 then '60-80%'
-            when between 0.8 and 1 then '80-100%'
+        case 
+            when import_pct between 0 and 0.2 then '0-20%'
+            when import_pct between 0.2 and 0.4 then '20-40%'
+            when import_pct between 0.4 and 0.6 then '40-60%'
+            when import_pct between 0.6 and 0.8 then '60-80%'
+            when import_pct between 0.8 and 1 then '80-100%'
             else 'N/A'
         end as binned_import_pct,
-        case export_pct
-            when between 0 and 0.2 then '0-20%'
-            when between 0.2 and 0.4 then '20-40%'
-            when between 0.4 and 0.6 then '40-60%'
-            when between 0.6 and 0.8 then '60-80%'
-            when between 0.8 and 1 then '80-100%'
+        case 
+            when export_pct between 0 and 0.2 then '0-20%'
+            when export_pct between 0.2 and 0.4 then '20-40%'
+            when export_pct between 0.4 and 0.6 then '40-60%'
+            when export_pct between 0.6 and 0.8 then '60-80%'
+            when export_pct between 0.8 and 1 then '80-100%'
             else 'N/A'
-        end as binned_export_pct
+        end as binned_export_pct,
+        case when bc.port_code is not null then 'Border' else 'Non-Border' end as port_type
     from
         bcbdb.public.principal_ports pp
-        join bcbdb.public.border_crossings bc on pp.port = bc.port_code
+        left join bcbdb.public.border_crossings bc on pp.port = bc.port_code
 );
 
 DROP TABLE IF EXISTS bcbdb.prod.port_monthly_traffic;
