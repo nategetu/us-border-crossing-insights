8d3675502d25d660a2cca1ffa409185bf98c7179
Nate Getu <nathanielgetu@gmail.com>
2025-04-17 22:09:57 -0400

    some pre-processing for s3 to redshift

--- airflow/dags/common/data_ingestion.py
+++ airflow/dags/common/data_ingestion.py
@@ -18,20 +18,25 @@ def process_file(url, file_name):
         elif file_name == 'port_statistical_areas':
             pd_data = pd_data[['PORTIDPK', 'FEATUREDESCRIPTION']]
         pa_table = pa.Table.from_pandas(pd_data)
-        pq.write_table(pa_table, f'{file_name}_s3.parquet')
+        pq.write_table(pa_table, f'{file_name}.parquet')
 
 def parquet_to_s3(file_name, bucket, hook):
-    response = hook.load_file(filename=f'{file_name}_s3.parquet', key=f'{file_name}.parquet', bucket_name=bucket, replace=True)
-    if response is None and os.path.exists(file_name):
-        os.remove(file_name)
+    response = hook.load_file(filename=f'{file_name}.parquet', key=f'{file_name}.parquet', bucket_name=bucket, replace=True)
 
-def create_redshift_table(file_name):
-    engine = create_engine(f'postgresql://airflow:airflow@localhost:5432/airflow')
-    engine.connect()
-    pd_table = pq.read_table(f'{file_name}.parquet').to_pandas()
-    schema = pd.io.sql.get_schema(pd_table, name=file_name, con=engine)
-    return schema
 
+def create_redshift_table(filename):
+    try:
+        file_name = filename
+        engine = create_engine(f'postgresql+psycopg2://airflow:airflow@postgres/airflow')
+        engine.connect()
+        pd_table = pq.read_table(f'{file_name}.parquet').to_pandas()
+        pd_table.columns = [col.replace(" ", "_") for col in pd_table.columns]
+        drop_table_query = f"DROP TABLE IF EXISTS {file_name};"
+        schema = pd.io.sql.get_schema(pd_table, name=file_name, con=engine)
+        schema = schema.replace(" TEXT", " VARCHAR(4096)")
+        return drop_table_query+schema
+    except FileNotFoundError as e:
+        print(f"Error: {e}. {file_name}.parquet not found.")
 
 def cleanup_files(file_name):
     if os.path.exists(file_name):
