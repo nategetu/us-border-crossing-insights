7de24e3e3e9ab485f3d17d72f4564a03c472f8ca
Nate Getu <nathanielgetu@gmail.com>
2025-04-17 23:11:57 -0400

    pick bins to port tonnage query

--- airflow/dags/sql/transform_tables.sql
+++ airflow/dags/sql/transform_tables.sql
@@ -11,7 +11,35 @@ create table bcbdb.prod.port_tonnage as (
         cast(pp.foreign_ as FLOAT) / cast(pp.total as FLOAT) as foreign_pct,
         cast(pp.domestic as FLOAT) / cast(pp.total as FLOAT) as domestic_pct,
         cast(pp.imports as FLOAT) / cast(pp.total as FLOAT) as import_pct,
-        cast(pp.exports as FLOAT) / cast(pp.total as FLOAT) as export_pct
+        cast(pp.exports as FLOAT) / cast(pp.total as FLOAT) as export_pct,
+        case foreign_pct
+            when between 0 and 0.2 then '0-20%'
+            when between 0.2 and 0.4 then '20-40%'
+            when between 0.4 and 0.6 then '40-60%'
+            when between 0.6 and 0.8 then '60-80%'
+            when between 0.8 and 1 then '80-100%'
+        end as binned_foreign_pct,
+        case domestic_pct
+            when between 0 and 0.2 then '0-20%'
+            when between 0.2 and 0.4 then '20-40%'
+            when between 0.4 and 0.6 then '40-60%'
+            when between 0.6 and 0.8 then '60-80%'
+            when between 0.8 and 1 then '80-100%'
+        end as binned_domestic_pct,
+        case import_pct
+            when between 0 and 0.2 then '0-20%'
+            when between 0.2 and 0.4 then '20-40%'
+            when between 0.4 and 0.6 then '40-60%'
+            when between 0.6 and 0.8 then '60-80%'
+            when between 0.8 and 1 then '80-100%'
+        end as binned_import_pct,
+        case export_pct
+            when between 0 and 0.2 then '0-20%'
+            when between 0.2 and 0.4 then '20-40%'
+            when between 0.4 and 0.6 then '40-60%'
+            when between 0.6 and 0.8 then '60-80%'
+            when between 0.8 and 1 then '80-100%'
+        end as binned_export_pct
     from
         bcbdb.public.principal_ports pp
         join bcbdb.public.border_crossings bc on pp.port = bc.port_code
